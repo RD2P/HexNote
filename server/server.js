9aa53ff0929b5ee4fadc31d9e0034fb4ae89794d
Raphael <graphael.dr@gmail.com>
2023-10-18 00:56:14 -0600

    Added comments, removed old comments

--- server/server.js
+++ server/server.js
@@ -8,22 +8,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// app.get('/api', (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// })
 const dataFolder = path.join(__dirname, 'data')
 
-
+//reads files in the data folder, where encoded notes are stored, an serves to client
 app.get('/api/notes', (req,res) => {
   let notes = []
   fs.readdir(dataFolder, (err, files) => {
     if (err) throw err
     files.forEach(file => notes.push(file))
-    // console.log(notes)
     res.send(notes)
   })
 })
 
+//handles POST, makes new file for encoded note with date as title and saves to data/
 app.post('/api/test', (req, res) => {
   const receivedData = JSON.stringify(req.body);
   const title = date.format(new Date(), 'DD MMM HH-mm-ss')
